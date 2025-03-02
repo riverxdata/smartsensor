c6abf3393e405e6860b0727e00c5249aa3508c65
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-03-02 21:00:10 +0700

    uncmt

--- smartsensor/process/roi.py
+++ smartsensor/process/roi.py
@@ -36,20 +36,20 @@ def get_r2frame(
     high_val = threshold[1]
 
     # process each image in the input directory
-    # for image_location in glob.glob(os.path.join(data, "*.jpg"), recursive=True):
-    #     image = cv2.imread(image_location)
-    #     file_name = os.path.basename(image_location)
+    for image_location in glob.glob(os.path.join(data, "*.jpg"), recursive=True):
+        image = cv2.imread(image_location)
+        file_name = os.path.basename(image_location)
 
-    #     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    #     mask = cv2.inRange(image_hsv, low_val, high_val)
-    #     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #     contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    #     if not contours:
-    #         raise ValueError("Your image does not have any countours")
-    #     x, y, w, h = cv2.boundingRect(contours[0])
-    #     roi = image[y : y + h, x : x + w]
-    #     roi = cv2.resize(roi, (RESIZE_DIM, RESIZE_DIM), interpolation=cv2.INTER_AREA)
-    #     cv2.imwrite(os.path.join(r2_path, file_name), roi)
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(image_hsv, low_val, high_val)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        if not contours:
+            raise ValueError("Your image does not have any countours")
+        x, y, w, h = cv2.boundingRect(contours[0])
+        roi = image[y : y + h, x : x + w]
+        roi = cv2.resize(roi, (RESIZE_DIM, RESIZE_DIM), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(r2_path, file_name), roi)
     return r2_path
 
 
@@ -72,24 +72,24 @@ def get_roi_background(
     center = RESIZE_DIM // 2
     half_size = ROI_DIM // 2
 
-    # for image_location in glob.glob(os.path.join(data, "*.jpg")):
-    #     image = cv2.imread(image_location)
-    #     file_name = os.path.basename(image_location)
-    #     # roi
-    #     roi = image[
-    #         center - half_size : center + half_size,
-    #         center - half_size : center + half_size,
-    #     ]
+    for image_location in glob.glob(os.path.join(data, "*.jpg")):
+        image = cv2.imread(image_location)
+        file_name = os.path.basename(image_location)
+        # roi
+        roi = image[
+            center - half_size : center + half_size,
+            center - half_size : center + half_size,
+        ]
 
-    #     cv2.imwrite(os.path.join(roi_path, file_name), roi)
-    #     rgb2dataframe(roi).to_csv(
-    #         os.path.join(roi_path, file_name.replace(".jpg", ".csv")), index=False
-    #     )
+        cv2.imwrite(os.path.join(roi_path, file_name), roi)
+        rgb2dataframe(roi).to_csv(
+            os.path.join(roi_path, file_name.replace(".jpg", ".csv")), index=False
+        )
 
-    #     # background
-    #     quarter_size = half_size // 2
-    #     background = image[
-    #         center - half_size : center + half_size, half_size : half_size * 3
-    #     ]
-    #     cv2.imwrite(os.path.join(bg_path, file_name), background)
+        # background
+        quarter_size = half_size // 2
+        background = image[
+            center - half_size : center + half_size, half_size : half_size * 3
+        ]
+        cv2.imwrite(os.path.join(bg_path, file_name), background)
     return roi_path, bg_path
